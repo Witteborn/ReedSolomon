a77a2307cd96bd0f067c6c813a612f50cde846a2
Gökhan Witteborn-Demir <Witteborn@protonmail.com>
2023-11-20 21:44:28 +0100

    Uses .NET 8 Syntax features

--- ReedSolomon/ReedSolomonTests/ReedSolomonTests.cs
+++ ReedSolomon/ReedSolomonTests/ReedSolomonTests.cs
@@ -5,19 +5,13 @@ namespace ReedSolomonTests
     [TestClass()]
     public class ReedSolomonTests
     {
-        //[TestMethod()]
-        //public void ReedSolomonTest()
-        //{
-        //    Assert.Fail();
-        //}
-
         [TestMethod()]
         public void EncodeParityTest()
         {
             //Arrange
             ReedSolomon rs = new ReedSolomon(dataShardCount: 4, parityShardCount: 2);
 
-            sbyte[] data = new sbyte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            sbyte[] data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
             List<sbyte[]> shardsList = new();
             for (int i = 0; i < 4; i++)
@@ -40,15 +34,15 @@ namespace ReedSolomonTests
 
 
             //Assert
-            sbyte[][] expectedShards = new sbyte[][]
-{
-                new sbyte[] {0,1,2,3 },
-                new sbyte[] {4,5,6,7},
-                new sbyte[] {8,9,10,11 },
-                new sbyte[] {12,13,14,15 },
-                new sbyte[] {16,17,18,19 },
-                new sbyte[] {20,21,22,23 }
-};
+            sbyte[][] expectedShards =
+            [
+                [0, 1, 2, 3],
+                [4, 5, 6, 7],
+                [8, 9, 10, 11],
+                [12, 13, 14, 15],
+                [16, 17, 18, 19],
+                [20, 21, 22, 23]
+            ];
 
             for (int i = 0; i < shards.Length; i++)
             {
@@ -62,34 +56,34 @@ namespace ReedSolomonTests
             //Arrange
             ReedSolomon rs = new ReedSolomon(dataShardCount: 4, parityShardCount: 2);
 
-            sbyte[][] shards = new sbyte[][]
-            {
-                new sbyte[] {0,1,2,3 },
-                //new sbyte[] {4,5,6,7},
-                new sbyte[] {0,0,0,0},
-                new sbyte[] {8,9,10,11 },
-                new sbyte[] {12,13,14,15 },
-                new sbyte[] {16,17,18,19 },
-                new sbyte[] {20,21,22,23 }
-            };
+            sbyte[][] shards =
+            [
+                [0, 1, 2, 3],
+                //[4, 5, 6, 7],
+                [0, 0, 0, 0],
+                [8, 9, 10, 11],
+                [12, 13, 14, 15],
+                [16, 17, 18, 19],
+                [20, 21, 22, 23]
+            ];
 
 
-            bool[] shardPresent = new bool[] { true, false, true, true, true, true };
+            bool[] shardPresent = [true, false, true, true, true, true];
             //Act
             rs.DecodeMissing(shards, shardPresent, 0, shards[0].Length);
 
 
             //Assert
 
-            sbyte[][] expectedShards = new sbyte[][]
-            {
-                new sbyte[] {0,1,2,3 },
-                new sbyte[] {4,5,6,7},
-                new sbyte[] {8,9,10,11 },
-                new sbyte[] {12,13,14,15 },
-                new sbyte[] {16,17,18,19 },
-                new sbyte[] {20,21,22,23 }
-            };
+            sbyte[][] expectedShards =
+            [
+                [0, 1, 2, 3],
+                [4, 5, 6, 7],
+                [8, 9, 10, 11],
+                [12, 13, 14, 15],
+                [16, 17, 18, 19],
+                [20, 21, 22, 23]
+            ];
 
             for (int i = 0; i < shards.Length; i++)
             {
@@ -103,35 +97,35 @@ namespace ReedSolomonTests
             //Arrange
             ReedSolomon rs = new ReedSolomon(dataShardCount: 4, parityShardCount: 2);
 
-            sbyte[][] shards = new sbyte[][]
-            {
-                new sbyte[] {0,1,2,3 },
-                //new sbyte[] {4,5,6,7},
-                new sbyte[] {0,0,0,0},
-                new sbyte[] {8,9,10,11 },
-                new sbyte[] {0,0,0,0},
-                //new sbyte[] {12,13,14,15 },
-                new sbyte[] {16,17,18,19 },
-                new sbyte[] {20,21,22,23 }
-            };
+            sbyte[][] shards =
+            [
+                [0, 1, 2, 3],
+                //[4, 5, 6, 7],
+                [0, 0, 0, 0],
+                [8, 9, 10, 11],
+                [0, 0, 0, 0],
+                //[12, 13, 14, 15],
+                [16, 17, 18, 19],
+                [20, 21, 22, 23]
+            ];
 
 
-            bool[] shardPresent = new bool[] { true, false, true, false, true, true };
+            bool[] shardPresent = [true, false, true, false, true, true];
             //Act
             rs.DecodeMissing(shards, shardPresent, 0, shards[0].Length);
 
 
             //Assert
 
-            sbyte[][] expectedShards = new sbyte[][]
-            {
-                new sbyte[] {0,1,2,3 },
-                new sbyte[] {4,5,6,7},
-                new sbyte[] {8,9,10,11 },
-                new sbyte[] {12,13,14,15 },
-                new sbyte[] {16,17,18,19 },
-                new sbyte[] {20,21,22,23 }
-            };
+            sbyte[][] expectedShards =
+            [
+                [0, 1, 2, 3],
+                [4, 5, 6, 7],
+                [8, 9, 10, 11],
+                [12, 13, 14, 15],
+                [16, 17, 18, 19],
+                [20, 21, 22, 23]
+            ];
 
             for (int i = 0; i < shards.Length; i++)
             {
@@ -145,34 +139,34 @@ namespace ReedSolomonTests
             //Arrange
             ReedSolomon rs = new ReedSolomon(dataShardCount: 4, parityShardCount: 2);
 
-            sbyte[][] shards = new sbyte[][]
-            {
-                new sbyte[] {0,1,2,3 },
-                new sbyte[] {4,5,6,7},
-                new sbyte[] {8,9,10,11 },
-                new sbyte[] {12,13,14,15 },
-                //new sbyte[] {16,17,18,19 },
-                new sbyte[] {0,0,0,0},
-                new sbyte[] {20,21,22,23 }
-            };
+            sbyte[][] shards =
+            [
+                [0, 1, 2, 3],
+                [4, 5, 6, 7],
+                [8, 9, 10, 11],
+                [12, 13, 14, 15],
+                //[16, 17, 18, 19],
+                [0, 0, 0, 0],
+                [20, 21, 22, 23]
+            ];
 
 
-            bool[] shardPresent = new bool[] { true, true, true, true, false, true };
+            bool[] shardPresent = [true, true, true, true, false, true];
             //Act
             rs.DecodeMissing(shards, shardPresent, 0, shards[0].Length);
 
 
             //Assert
 
-            sbyte[][] expectedShards = new sbyte[][]
-            {
-                new sbyte[] {0,1,2,3 },
-                new sbyte[] {4,5,6,7},
-                new sbyte[] {8,9,10,11 },
-                new sbyte[] {12,13,14,15 },
-                new sbyte[] {16,17,18,19 },
-                new sbyte[] {20,21,22,23 }
-            };
+            sbyte[][] expectedShards =
+            [
+                [0, 1, 2, 3],
+                [4, 5, 6, 7],
+                [8, 9, 10, 11],
+                [12, 13, 14, 15],
+                [16, 17, 18, 19],
+                [20, 21, 22, 23]
+            ];
 
             for (int i = 0; i < shards.Length; i++)
             {
@@ -186,35 +180,35 @@ namespace ReedSolomonTests
             //Arrange
             ReedSolomon rs = new ReedSolomon(dataShardCount: 4, parityShardCount: 2);
 
-            sbyte[][] shards = new sbyte[][]
-            {
-                new sbyte[] {0,1,2,3 },
-                new sbyte[] {4,5,6,7},
-                new sbyte[] {8,9,10,11 },
-                new sbyte[] {12,13,14,15 },
-                //new sbyte[] {16,17,18,19 },
-                new sbyte[] {0,0,0,0},
-                //new sbyte[] {20,21,22,23 }
-                new sbyte[] {0,0,0,0}
-            };
+            sbyte[][] shards =
+            [
+                [0, 1, 2, 3],
+                [4, 5, 6, 7],
+                [8, 9, 10, 11],
+                [12, 13, 14, 15],
+                //[16, 17, 18, 19],
+                [0, 0, 0, 0],
+                //[20, 21, 22, 23]
+                [0, 0, 0, 0]
+            ];
 
 
-            bool[] shardPresent = new bool[] { true, true, true, true, false, false };
+            bool[] shardPresent = [true, true, true, true, false, false];
             //Act
             rs.DecodeMissing(shards, shardPresent, 0, shards[0].Length);
 
 
             //Assert
 
-            sbyte[][] expectedShards = new sbyte[][]
-            {
-                new sbyte[] {0,1,2,3 },
-                new sbyte[] {4,5,6,7},
-                new sbyte[] {8,9,10,11 },
-                new sbyte[] {12,13,14,15 },
-                new sbyte[] {16,17,18,19 },
-                new sbyte[] {20,21,22,23 }
-            };
+            sbyte[][] expectedShards =
+            [
+                [0, 1, 2, 3],
+                [4, 5, 6, 7],
+                [8, 9, 10, 11],
+                [12, 13, 14, 15],
+                [16, 17, 18, 19],
+                [20, 21, 22, 23]
+            ];
 
             for (int i = 0; i < shards.Length; i++)
             {
@@ -229,19 +223,19 @@ namespace ReedSolomonTests
             //Arrange
             ReedSolomon rs = new ReedSolomon(dataShardCount: 4, parityShardCount: 2);
 
-            sbyte[][] shards = new sbyte[][]
-            {
-                new sbyte[] {0,0,0,0},
-                new sbyte[] {0,0,0,0},
-                new sbyte[] {0,0,0,0},
-                new sbyte[] {0,0,0,0},
+            sbyte[][] shards =
+            [
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
 
-                new sbyte[] {0,0,0,0},
-                new sbyte[] {0,0,0,0}
-            };
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ];
 
 
-            bool[] shardPresent = new bool[] { false, false, false, false, false, false };
+            bool[] shardPresent = [false, false, false, false, false, false];
             //Act
             var ex = Assert.ThrowsException<ArgumentException>(() => rs.DecodeMissing(shards, shardPresent, 0, shards[0].Length));
 
